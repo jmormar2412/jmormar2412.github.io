3c218d4264ca6cc84df590b08a16848ce0c5bbaf
jmormar2412 <jmormar2412@g.educaand.es>
2023-05-22 09:31:38 +0200

    más modificaciones y comentarios

--- js/comprobarContraseña.js
+++ js/comprobarContraseña.js
@@ -1,22 +1,25 @@
-window.onload = function() {
+/**
+ * Función para la comprobación de la contraseña y cambiar color de los campos que se cumplan.
+ */
+window.onload = function () {
     let passwordField = document.getElementById("pass");
-  
+
     passwordField.addEventListener("input", function () {
         let password = passwordField.value;
-  
+
         let lengthIndicator = document.querySelector('span[data-indicator="length"]');
         let numberIndicator = document.querySelector('span[data-indicator="number"]');
         let uppercaseIndicator = document.querySelector('span[data-indicator="uppercase"]');
         let specialIndicator = document.querySelector('span[data-indicator="special"]');
-  
+
         let result = checkPasswordStrength(password);
-  
+
         lengthIndicator.style.color = result.length ? "green" : "red";
         uppercaseIndicator.style.color = result.uppercase ? "green" : "red";
         numberIndicator.style.color = result.number ? "green" : "red";
         specialIndicator.style.color = result.special ? "green" : "red";
     });
-  
+
     function checkPasswordStrength(password) {
         let result = {
             length: password.length >= 8 && password.length <= 16,
@@ -27,8 +30,10 @@ window.onload = function() {
         return result;
     }
 }
-  
 
+/**
+ * Función para comprobar que no se dejen campos en blanco.
+ */
 function comprobarCosas() {
 
     var name = document.getElementById("nombreUser").value;
@@ -44,7 +49,7 @@ function comprobarCosas() {
             text: 'Habrás dejado algún campo en blanco'
         })
     }
-    else if(pass != passRep){
+    else if (pass != passRep) {
         Swal.fire({
             icon: 'error',
             title: 'Erraaso...',
@@ -58,27 +63,30 @@ function comprobarCosas() {
             text: 'Acepta los términos del servicio'
         })
     }
-    else{
+    else {
         obj = {
             nombreUser: name,
             correo: correo,
             pass: pass,
             passRep: passRep
         }
-        
+
         Swal.fire({
             icon: 'success',
             title: 'Pefe',
-            text: 'el objeto será '+ JSON.stringify(obj)
+            text: 'el objeto será ' + JSON.stringify(obj)
         })
     }
 }
 
-function politicaContraseña(){
+/**
+ * Función para mostrar la política de contraseñas (caracteres especiales permitidos para la contraseña.)
+ */
+function politicaContraseña() {
     Swal.fire({
         title: 'Política de contraseñas',
         html: '• Debe contener entre 8 y 16 caracteres.<br>• Debe tener al menos un número<br>• Debe tener al menos una mayúscula<br>• Debe tener al menos un caracter especial:<br> !@#$%^&*()_+-=[]{};:"\\|,.<>\/?\'',
         icon: 'info',
         confirmButtonText: 'Aceptar'
-      });
+    });
 }
